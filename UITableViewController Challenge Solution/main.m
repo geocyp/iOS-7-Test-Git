3013644653a50f903ed9391c1b658fd47619f81e
jio <jio@jios-Mac-Pro.local>
2014-08-30 09:02:36 +0300

    Initial Commit

---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by jio on 30/08/14.
+//  Copyright (c) 2014 Jio Gi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JGiAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JGiAppDelegate class]));
+    }
+}
